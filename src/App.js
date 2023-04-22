a17363a868be43455a3d6cc4085372579f784ad2
elahemortazavi <emor5680@gmail.com>
2023-04-22 14:16:13 +0100

    App

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import './App.css';
+const App = () => {
+  const content = [
+    {
+      title: "Aliqua pariatur",
+      description: "Occaecat cupidated id",
+      img: "m1.jpg",
+    },
+    {
+      title: "Comodo pariatur",
+      description: "Occaecat cupidated id",
+      img: "m2.jpg",
+    },
+    {
+      title: "Enim pariatur",
+      description: "Occaecat cupidated id",
+      img: "m3.jpg",
+    },
+  ];
+
+
+
+
+  return (
+    <div className="App">
+      <Carousel content = {content} />
+    </div>
+  );
+  };
+
+export default App;
