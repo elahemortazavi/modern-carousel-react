56206f3e1ec71ffbb9ffcc951bef1fd120d25cfa
elahemortazavi <emor5680@gmail.com>
2023-04-23 19:46:15 +0100

    carousel

---
+++ src/Carousel.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+const [currentContentIndex, setCurrentIndex] = useState(0);
+const [transition, setTransition] = useState(false);
+
+const nextImage = () => {
+    setTransition(true);
+    setTimeout(() => {
+        setCurrentContentIndex((currentContentIndex + 1) % content.length);
+        setTransition(false)
+    }, 500);
+};
+const prevImage = () => {
+    setTransition(true);
+    setTimeout(() => {
+        setCurrentContentIndex (
+        setCurrentContentIndex === 0 ? content.length - 1 : currentContentIndex - 1);
+        setTransition(false);
+    }, 500);
+}
+ return
